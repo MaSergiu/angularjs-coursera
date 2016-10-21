ec26397d4b9bf56d7da63c7618bdfeb356c25c10
MaSergiu <masergiu@gmail.com>
2016-10-21 07:32:22 +0300

    Validated the form

--- assignments/assignment5/src/public/newsletter/sign-up.controller.js
+++ assignments/assignment5/src/public/newsletter/sign-up.controller.js
@@ -8,9 +8,20 @@
     function signUpController (menuCategories) {
         var controller = this;
 
+        controller.nameMinLength = 4;
+
+        controller.user = {
+            firstName: '',
+            lastName: '',
+            email: '',
+            phone: '',
+            favouriteDish: ''
+        };
+
         controller.menuCategories = menuCategories;
 
         controller.submit = function () {
+            console.log(controller.user);
             alert('SUBMITTED');
         }
 
